d07539be35414f1b20ce35a7942162eb72d9b12e
Daniel Perez <dperez8@njcu.edu>
2021-09-30 16:09:50 +0000

    second commit

--- SayHi.java
+++ SayHi.java
@@ -1,5 +1,9 @@
 public class SayHi{
 	public static void main(String[] args){
 		System.out.println("Say hi");
+		System.out.println("Say hi");
+		System.out.println("Say hi");
+		System.out.println("Say hi");
+		System.out.println("Say hi");
 	} 
 }	
